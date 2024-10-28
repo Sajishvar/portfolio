cd2787c24e8fd6eb752490c06159376ab0b76d17
Sajishvar <mohansajishvar@gmail.com>
2024-10-28 11:02:42 +0530

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,11 +4,11 @@
         <div class="col-md">
         <div class="ftco-footer-widget mb-4">
             <h2 class="ftco-heading-2">About</h2>
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p>Blending creativity and technology, I design user-focused, visually appealing websites. I transform ideas into impactful digital experiences.</p>
             <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+            <li class="ftco-animate"><a href="https://www.linkedin.com/in/sajishvar-mohan-11a1081b1" target="blank"><span class="icon-linkedin"></span></a></li>
+            <li class="ftco-animate"><a href="https://github.com/Sajishvar" target="blank"><span class="icon-github"></span></a></li>
+            <li class="ftco-animate"><a href="https://www.facebook.com/profile.php?id=100011324879618&mibextid=JRoKGi" target="blank"><span class="icon-facebook"></span></a></li>
             </ul>
         </div>
         </div>
@@ -28,22 +28,22 @@
             <div class="ftco-footer-widget mb-4">
             <h2 class="ftco-heading-2">Services</h2>
             <ul class="list-unstyled">
-            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Design</a></li>
-            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Development</a></li>
-            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Business Strategy</a></li>
-            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Data Analysis</a></li>
-            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Graphic Design</a></li>
+            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Front-End Development</a></li>
+            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Back-End Development</a></li>
+            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>SEO Optimization</a></li>
+            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Website Maintenance</a></li>
+            <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Custom Web Design</a></li>
             </ul>
         </div>
         </div>
         <div class="col-md">
         <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Have a Questions?</h2>
+            <h2 class="ftco-heading-2">Reach me</h2>
             <div class="block-23 mb-3">
                 <ul>
-                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                <li><span class="icon icon-map-marker"></span><span class="text">201/131, Jeyaraja Mawatha, Kotagala,<br> Nuwara Eliya,<br>Sri Lanka</span></li>
+                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+94 76 8262 123</span></a></li>
+                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">mohansajishvar@gmail.com</span></a></li>
                 </ul>
             </div>
         </div>
@@ -52,9 +52,8 @@
     <div class="row">
         <div class="col-md-12 text-center">
 
-        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        <p>
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
         </div>
     </div>
     </div>
