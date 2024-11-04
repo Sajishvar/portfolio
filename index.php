70a8fc268ccfd0dd7fd91659c783736e9993a174
Sajishvar <mohansajishvar@gmail.com>
2024-11-05 01:22:47 +0530

    Skills

--- index.php
+++ index.php
@@ -182,6 +182,108 @@ ob_start();
 	</div>
 </section>
 
+<section class="ftco-section" id="skills-section">
+	<div class="container">
+		<div class="row justify-content-center pb-5">
+			<div class="col-md-12 heading-section text-center ftco-animate">
+				<h1 class="big big-2">Skills</h1>
+				<h2 class="mb-4">My Skills</h2>
+				<p>Skilled in web development and software engineering, with a solid foundation in HTML, CSS, JavaScript, PHP, and expertise in creating efficient, user-centered digital solutions.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>HTML & CSS</h3>
+					<div class="progress">
+						<div class="progress-bar color-1" role="progressbar" aria-valuenow="90"
+						aria-valuemin="0" aria-valuemax="100" style="width:90%">
+						<span>90%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>Javascript</h3>
+					<div class="progress">
+						<div class="progress-bar color-2" role="progressbar" aria-valuenow="85"
+						aria-valuemin="0" aria-valuemax="100" style="width:85%">
+						<span>85%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>PHP</h3>
+					<div class="progress">
+						<div class="progress-bar color-3" role="progressbar" aria-valuenow="60"
+						aria-valuemin="0" aria-valuemax="100" style="width:60%">
+						<span>60%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>Laravel</h3>
+					<div class="progress">
+						<div class="progress-bar color-4" role="progressbar" aria-valuenow="50"
+						aria-valuemin="0" aria-valuemax="100" style="width:50%">
+						<span>50%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>Java</h3>
+					<div class="progress">
+						<div class="progress-bar color-5" role="progressbar" aria-valuenow="70"
+						aria-valuemin="0" aria-valuemax="100" style="width:70%">
+						<span>70%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>C++</h3>
+					<div class="progress">
+						<div class="progress-bar color-6" role="progressbar" aria-valuenow="80"
+						aria-valuemin="0" aria-valuemax="100" style="width:80%">
+						<span>80%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>SQL</h3>
+					<div class="progress">
+						<div class="progress-bar color-5" role="progressbar" aria-valuenow="65"
+						aria-valuemin="0" aria-valuemax="100" style="width:65%">
+						<span>65%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 animate-box">
+				<div class="progress-wrap ftco-animate">
+					<h3>SEO</h3>
+					<div class="progress">
+						<div class="progress-bar color-6" role="progressbar" aria-valuenow="85"
+						aria-valuemin="0" aria-valuemax="100" style="width:85%">
+						<span>85%</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php
 $pageContent = ob_get_clean();
 include("includes/layout.php");
